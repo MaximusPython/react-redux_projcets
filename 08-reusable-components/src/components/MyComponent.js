e051ec8b0c43a034d254613b83b3f79c69ab07b9
Maximus <85298325+MaximusPython@users.noreply.github.com>
2024-07-03 15:41:24 +0300

    sent-file-08

---
+++ 08-reusable-components/src/components/MyComponent.js
@@ -0,0 +1,10 @@
+function MyComponent() {
+  return (
+    <div>
+      <h1>hello from MyComponent!</h1>
+      <button>Like!</button>
+    </div>
+  )
+}
+
+export default MyComponent
---
+++ 08-reusable-components/src/components/OtherComponent.js
@@ -0,0 +1,12 @@
+function OtherComponent() {
+  return (
+    <div>
+      <p>
+        Hi this my test project. Hi this my test project. Hi this my test
+        project.
+      </p>
+    </div>
+  )
+}
+
+export default OtherComponent
