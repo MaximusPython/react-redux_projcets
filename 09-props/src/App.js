a2656b03390fead3099c4ec288c0202f14d6d7be
Maximus <85298325+MaximusPython@users.noreply.github.com>
2024-07-26 17:00:17 +0300

    sent-file-09

---
+++ 09-props/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import PetInfo from './components/PetInfo'
+
+function App() {
+  return (
+    <div className="App">
+      <PetInfo animal="parent" age="2" />
+    </div>
+  )
+}
+
+export default App
